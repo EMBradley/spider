86401c41b8f9663c41f3aeeeebff4162f9b4ae9c
Ethan Bradley <ethan.m.bradley@gmail.com>
2024-07-09 21:33:26 -0400

    reverse sort order

--- report.ts
+++ report.ts
@@ -8,7 +8,7 @@ async function printReport(url: URL) {
   const pages = await crawlPage(url, url, new Map());
   console.log(`Printing crawl report for ${url}:\n`);
 
-  const sortedPages = [...pages.entries()].sort((a, b) => b[1] - a[1]);
+  const sortedPages = [...pages.entries()].sort((a, b) => a[1] - b[1]);
   for (const [page, count] of sortedPages) {
     console.log(`Found ${count} internal links to ${page}`);
   }
